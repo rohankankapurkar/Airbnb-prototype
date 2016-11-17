69d7ada35b8584aa5ce7482a37227c2663525b71
darshansapaliga <darshanvaman.sapaliga@sjsu.edu>
2016-11-16 20:32:23 -0800

    signin ui & contro UI & controller

--- Airbnb-Client/public/scripts/controller.home.js
+++ Airbnb-Client/public/scripts/controller.home.js
@@ -1,13 +1,45 @@
 
 airbnbApp.controller('controllerHome',function($scope,$log){
-	
+
 	$scope.check = "";
 	$scope.hmpgwhere = "";
-	
+
+	//hide signInError message on get signin page
+	$scope.signInError = false;
+
 	$scope.test = function()
 	{
 		$log.info("hello");
 		$log.info($scope.hmpgwhere);
 	}
-	
+
+	/*
+   |-----------------------------------------------------------
+   | User signin
+   |-----------------------------------------------------------
+  */
+  $scope.signin = function() {
+    $http({
+      method : "POST",
+      url : '/user/signin',
+      data : {
+        "email": $scope.email,
+        "password": $scope.password
+      }
+    }).success(function(data) {
+      if(data == 0) {
+        window.location = '/';
+			}
+      else {
+				//code to show error for signin
+				$scope.signInError = true;
+			}
+    }).error(function(error) {
+			alert("Internal sever error occured");
+			window.setTimeout(function(){
+				window.location = '/';
+			}, 3000);
+    });
+  };
+
 })
