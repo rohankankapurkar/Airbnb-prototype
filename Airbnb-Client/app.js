d0e1a8b286652ee444153c1fc0ca9868c16463f0
Ajay <ajay.tanpure@sjsu.edu>
2016-11-16 19:37:46 -0800

    Updated app.js to use passport, loggings and sessions

--- Airbnb-Client/app.js
+++ Airbnb-Client/app.js
@@ -9,7 +9,46 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+/**
+ * passport dependencies
+ */
+passport = require('passport');
+   
+/**
+ * Session dependencies
+ */
+session = require('express-session')
+
+   
+/**
+ * Utilities dependencies should be added after this
+ */
+mongo = require("./routes/utils/util.mongo");
+
+
+   
+/**
+ * All route dependencies
+ */
+var usersession = require('./routes/misc/misc.session'); //contains functions related to session management
+var analytics = require("./routes/misc/misc.analytics"); //contains functions related to logging of client activities
+   
+var mongoSessionConnectURL = "mongodb://localhost:27017/sessions";
+var mongoStore = require("connect-mongo")(session);
+
 var app = express();
+
+//session properties
+app.use(session({
+	secret: 'eBay_client_session',
+	resave : false,
+	saveUninitialized : false,
+	duration: 30 * 60 * 1000,    
+	activeDuration: 5 * 60 * 1000,
+	store: new mongoStore({  
+		url: mongoSessionConnectURL})
+	})
+);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +69,16 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.post('/getusersession',usersession.getSession);
+app.post('/analytics', analytics.logdata);
+
+
+
+mongo.connect(mongoSessionConnectURL, function(){  
+	console.log('Connected to mongo at: ' + mongoSessionConnectURL); 
+	http.createServer(app).listen(app.get('port'), function(){  
+		console.log('Express server listening on port ' + app.get('port'));  
+	});  
 });
+
+
