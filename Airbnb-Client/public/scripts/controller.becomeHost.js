779a4ba42d78b26b42cdcf2db7403c882009b49a
Shrutiloya <shrutiloya1@gmail.com>
2016-11-17 14:27:03 -0800

    committing Controller Header

---
+++ Airbnb-Client/public/scripts/controller.becomeHost.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Shruti Loya on 11/17/2016.
+ */
+/**
+ * Created by Shruti Loya on 11/17/2016.
+ */
+
+airbnbApp.controller('controllerBecomeHost',function($scope,$location,$log,$http){
+
+    /*$scope.becomeHost = function () {
+        $location.path('/becomeHost');
+    };*/
+
+})
