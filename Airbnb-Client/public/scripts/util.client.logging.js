7282b4f891f97f20f42a88b784fec262de95731a
darshansapaliga <darshanvaman.sapaliga@sjsu.edu>
2016-11-26 14:39:35 -0800

    user logging added to all pages && city search integrated with properties

--- Airbnb-Client/public/scripts/util.client.logging.js
+++ Airbnb-Client/public/scripts/util.client.logging.js
@@ -1,19 +1,21 @@
 $(document).ready(function()
 	{
-	    $(":button").click(function()
+	    $(":button, a").click(function()
 	    {
+				console.log("button clicked");
     		var viewURL = $(location).attr("pathname");
 			var clickid = this.id;
-			var currentdate = new Date(); 
+			console.log(clickid);
+			var currentdate = new Date();
 			var property = "NA";
 
     		var datetime = currentdate.getDate() + "/"
-            	    + (currentdate.getMonth()+1)  + "/" 
-                	+ currentdate.getFullYear() + " "  
-                	+ currentdate.getHours() + ":"  
-                	+ currentdate.getMinutes() + ":" 
+            	    + (currentdate.getMonth()+1)  + "/"
+                	+ currentdate.getFullYear() + " "
+                	+ currentdate.getHours() + ":"
+                	+ currentdate.getMinutes() + ":"
                 	+ currentdate.getSeconds();
-    		
+
             $.post("/analytics/clicks",
     			    {
     			        url : viewURL,
