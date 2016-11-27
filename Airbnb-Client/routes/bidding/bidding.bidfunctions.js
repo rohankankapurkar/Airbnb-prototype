d56b2af10fe7e148816d8c50a7a3761dd48ec8cb
Niranjan Tungatkar <niranjananil.tungatkar@sjsu.edu>
2016-11-27 14:57:02 -0800

    Missing biddin file

---
+++ Airbnb-Client/routes/bidding/bidding.bidfunctions.js
@@ -0,0 +1,58 @@
+
+var dateFormat = require('dateformat');
+var mq_client = require('../../rpc/client');
+
+exports.placebid = function(req,res){
+	var propertyid = req.param('propertyid');
+	var title = req.param('title');
+	var bidamount = req.param('bidamount');
+	var bidder = req.param('bidder');
+	updateBidLog(propertyid,title,bidamount,bidder,res);
+	updatePropertyCollection(propertyid,bidder,bidamount,res);
+}
+
+
+function updateBidLog(propertyid, title, bidamount, bidder,res)
+{
+	
+	//var BidLogString = product_id+" | "+bidder+" | "+bidamount+" | "+getCurrentTime()+"\n";
+	//logger.writeBidInfoLog(BidLogString);
+	console.log("in bid log");
+	var msg_payload = {
+			propertyid : propertyid,
+			title : title,
+			bidamount : bidamount,
+			bidder : bidder
+	}
+	mq_client.make_request('updateBidLog_queue',msg_payload, function(err,result){
+		if(result.err){
+			console.log('Not able to fetch User data');
+			res.send(result);
+		}
+		else 
+		{
+			res.send(result);
+		}  
+	});
+}
+
+function updatePropertyCollection(propertyid, bidder, bidamount,res)
+{
+	var msg_payload = {
+		propertyid : propertyid,
+		bidder : bidder,
+		bidamount : bidamount
+	}
+	console.log("in property collection log");
+	mq_client.make_request('updatePropertyBids_queue',msg_payload, function(err,result){
+		if(result.err){
+			console.log('Not able to fetch User data');
+			res.send(result);
+		}
+		else 
+		{
+			res.send(result);
+		}  
+	});
+}
+
