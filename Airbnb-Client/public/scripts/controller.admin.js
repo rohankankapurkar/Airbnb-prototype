84ef7e0055bf5accee99cdce2b42d5764aed2d92
Ajay <ajay.tanpure@sjsu.edu>
2016-11-28 14:42:23 -0800

    Added admin dash charts from database

--- Airbnb-Client/public/scripts/controller.admin.js
+++ Airbnb-Client/public/scripts/controller.admin.js
@@ -6,115 +6,162 @@ airbnbApp.controller('controllerAdmin',function($scope,$log,$http,$state,$stateP
 
 	// This is bar chart
 
-	var barChartData = {
-		labels: ["Property1", "Property2", "Property3", "Property4", "Property5", "Property6"],
-		datasets: [{
-			label: 'Revenue/Year ($)',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: [
-			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
-			'rgba(255, 206, 86, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(153, 102, 255, 0.2)',
-			'rgba(255, 159, 64, 0.2)'
-			],
 
-			borderWidth: 0
-		}]
-	}; 
+// Plot bar chart for property revenue
+		$http({
+			method : "POST",
+			url : '/admin/top_properties'
+		}).success(function(data) {
+			console.log('Hey' + data.data[0]['prop_id']);
 
-	//bar chart 1
-	var barChartElement = document.getElementById("admin-bar-chart");
-	var barChart = new Chart(barChartElement, {
-		type: 'bar',
-		data: barChartData,
-		options: {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
-				}]
+			var propData = data.data;
+			var counter = 0;
+			var props = [];
+			var propsCosts = [];
+			var propsColors = []			
+
+			
+			for(counter = 0; counter < propData.length; counter++ ){
+				props.push(propData[counter]['prop_id']);
+				propsCosts.push(propData[counter]['total']);
+				propsColors.push('rgba(54, 162, 235, 0.2)');
 			}
-		}
-	});
 
-	//bar chart 2
-	var barChartElement = document.getElementById("admin-bar-chart-1");
-	var barChart = new Chart(barChartElement, {
-		type: 'bar',
-		data: barChartData,
-		options: {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero:true
-					}
+			var barChartData = {
+				labels: props,
+				datasets: [{
+					label: 'Revenue/Year ($)',
+					data: propsCosts,
+					backgroundColor: propsColors,
+					borderWidth: 0
 				}]
+			};
+
+			var barChartElement = document.getElementById("admin-bar-chart");
+			var barChart = new Chart(barChartElement, {
+				type: 'bar',
+				data: barChartData,
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero:true
+							}
+						}]
+					}
+				}
+			});
+
+		}).error(function(error) {
+			return false;
+		});
+
+// Pie chart for city revenue
+
+	
+		$http({
+			method : "POST",
+			url : '/admin/top_cities'
+		}).success(function(data) {
+			console.log('Hey' + data.data[0]['prop_id']);
+
+			var propData = data.data;
+			var counter = 0;
+			var city = [];
+			var cityCosts = [];
+			var allColors = ['#FF6384','#36A2EB','#FFCE56','#FF6384','#36A2EB','#FFCE56','#FF6384','#36A2EB','#FFCE56','#FFCE56'];
+			var cityColors = [];
+			
+			for(counter = 0; counter < propData.length; counter++ ){
+				city.push(propData[counter]['city']);
+				cityCosts.push(propData[counter]['total']);
+				cityColors.push(allColors[counter]);
 			}
-		}
-	});
+
+				pieChartData = {
+				labels: city,
+				datasets: [
+				{
+					data: cityCosts,
+					borderWidth	:[0,0,0],
+					backgroundColor: cityColors,
+					hoverBackgroundColor: cityColors
+				}]
+			};
+			
+			var pieChartElement = document.getElementById('admin-pie-chart');
+			var myPieChart = new Chart(pieChartElement,{
+				type: 'doughnut',
+				animation:{
+		        animateScale:true
+		    	},
+				data: pieChartData,
+				options: {
+		     	   elements: {
+		        	    arc: {
+		            	    borderColor: "#000000"
+		            	}
+		       		 }
+		    	}
+			});
+
+		}).error(function(error) {
+			return false;
+		});
 
 
-	//This is pie chart
+// Plot bar chart for top host revenu
 
-	pieChartData = {
-		labels: [
-		"San Jose",
-		"New York",
-		"Pune"
-		],
-		datasets: [
-		{
-			data: [100, 250, 400],
-			borderWidth	:[0,0,0],
-			backgroundColor: [
-			"#FF6384",
-			"#36A2EB",
-			"#FFCE56"
-			],
-			hoverBackgroundColor: [
-			"#FF6384",
-			"#36A2EB",
-			"#FFCE56"
-			]
-		}]
-	};
+		$http({
+			method : "POST",
+			url : '/admin/top_hosts'
+		}).success(function(data) {
+			console.log('Hey' + data.data[0]['prop_id']);
 
-	//pie chart 1
-	var pieChartElement = document.getElementById('admin-pie-chart');
-	var myPieChart = new Chart(pieChartElement,{
-		type: 'doughnut',
-		animation:{
-        animateScale:true
-    	},
-		data: pieChartData,
-		options: {
-     	   elements: {
-        	    arc: {
-            	    borderColor: "#000000"
-            	}
-       		 }
-    	}
-	});
+			var propData = data.data;
+			var counter = 0;
+			var hosts = [];
+			var hostsCosts = [];
+			var hostsColors = []			
+
+			
+			for(counter = 0; counter < propData.length; counter++ ){
+				hosts.push(propData[counter]['host_id']);
+				hostsCosts.push(propData[counter]['total']);
+				hostsColors.push('rgba(54, 162, 235, 0.2)');
+			}
+
+			var barChartData = {
+				labels: hosts,
+				datasets: [{
+					label: 'Revenue/Year ($)',
+					data: hostsCosts,
+					backgroundColor: hostsColors,
+					borderWidth: 0
+				}]
+			};
+
+			var barChartElement = document.getElementById("admin-bar-chart-1");
+			var barChart = new Chart(barChartElement, {
+				type: 'bar',
+				data: barChartData,
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero:true
+							}
+						}]
+					}
+				}
+			});
+
+		}).error(function(error) {
+			return false;
+		});
 
 
-	//pie chart 1
-	var pieChartElement = document.getElementById('admin-pie-chart-1');
-	var myPieChart = new Chart(pieChartElement,{
-		type: 'doughnut',
-		animation:{
-        animateScale:true
-    	},
-		data: pieChartData,
-		options: {
-     	   elements: {
-        	    arc: {
-            	    borderColor: "#000000"
-            	}
-       		 }
-    	}
-	});	
+
+
 
 });
