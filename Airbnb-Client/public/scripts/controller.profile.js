77ef48d185e9a5f8e80139e167a573f0cb7822bc
rohankankapurkar <rohan.kankapurkar@gmail.com>
2016-11-30 23:49:57 -0800

    updated profile

--- Airbnb-Client/public/scripts/controller.profile.js
+++ Airbnb-Client/public/scripts/controller.profile.js
@@ -4,147 +4,91 @@ airbnbApp.controller('controllerProfile',function($scope,$log,$http,$state){
 
 $scope.inv_credit_card = "";
 
-	 $http({
-		method : "GET",
-		url : '/user/update_profile',
-//		data : {
+
+$http({
+	method : "GET",
+	url : '/user/update_profile',
+//	data : {
 //
-//			"username" : $scope.user_email,
+//		"username" : $scope.user_email,
 //
-//		}
-	}).success(function(data){
-		if (data.statuscode == 0)
-		{
-			console.log("got the show_prfile data"+data.username);
-			//printing the user name here babyslash@gmail.com{"_id":"5832d422402a63fcf41fc033","username":"slash@gmail.com","password":"f70feeaffcd3f592a4fb7812b7d86d","firstname":"bapu","lastname":"jamm","birthday":"2016-11-01","ishost":false,"id":"428-82-0311","lastLogin":"Mon Nov 21 2016 03:24:32 GMT-0800 (PST)","sex":"Female","phone":"1234","user_preferred_locale":"de","user_native_currency":"CLP","city":"lonfon","about":"man i aint cool"}
-
-			$scope.user_first_name = data.firstname;
-			$scope.user_last_name=data.lastname;
-			$scope.user_email = data.username;
-			$scope.sex=data.sex;
-			$scope.phone=data.phone;
-			$scope.city=data.city;
-			$scope.about=data.about;
-			$scope.user_native_currency= data.user_native_currency;
-			$scope.user_preferred_locale=data.user_preferred_locale;
-			$scope.user_creditcard = data.credit_card;
-			$scope.profile_pic = data.profile_pic;
-			$scope.birthday = data.birthday;
-
-		}
-		else
-		{
-			if(data.message != null)
-			{
-				$scope.invEmail = "";
-				$scope.invEmail = data.message;
-			}
-		}
-	}).error(function(error) {
-		console.log("error");
-	});
-
-		
-	 
-	 
-	 
-	 
-	 
-	 
-//	 $scope.update_Profile_pic = function()
-//		
-//		{
-//			$scope.profile_pic = $("#profile_pic").val();
-//			console.log($scope.profile_pic);
-//
-//			$http({
-//				method : "POST",
-//				url : '/user/update_profile_pic',
-//				data : {
-//				
-//					"profile_pic" :$scope.profile_pic
-//				}
-//			}).success(function(data){
-//				if (data.statuscode == 0)
-//				{
-//					console.log("updated the profile picture successfully");
-//				}
-//				else
-//				{
-//					if(data.message != null)
-//					{
-//						$scope.invEmail = "";
-//						$scope.invEmail = data.message;
-//					}
-//				}
-//			}).error(function(error) {
-//				console.log("error");
-//			});
-//
-//			
-//			
-//			
-//			
-//		}
-
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-
-	$scope.update_Profile = function()
-	
-	
+//	}
+}).success(function(data){
+	if (data.statuscode == 0)
 	{
-		$scope.inv_credit_card = "";
-		$scope.inv_phone = "";
+		console.log("got the show_prfile data"+data.username);
+		//printing the user name here babyslash@gmail.com{"_id":"5832d422402a63fcf41fc033","username":"slash@gmail.com","password":"f70feeaffcd3f592a4fb7812b7d86d","firstname":"bapu","lastname":"jamm","birthday":"2016-11-01","ishost":false,"id":"428-82-0311","lastLogin":"Mon Nov 21 2016 03:24:32 GMT-0800 (PST)","sex":"Female","phone":"1234","user_preferred_locale":"de","user_native_currency":"CLP","city":"lonfon","about":"man i aint cool"}
+
+		$scope.user_first_name = data.firstname;
+		$scope.user_last_name=data.lastname;
+		$scope.user_email = data.username;
+		$scope.sex=data.sex;
+		$scope.phone=data.phone;
+		$scope.city=data.city;
+		$scope.about=data.about;
+		$scope.user_native_currency= data.user_native_currency;
+		$scope.user_preferred_locale=data.user_preferred_locale;
+		$scope.user_creditcard = data.credit_card;
+		$scope.profile_pic = data.profile_pic;
+
+
+	}
+	else
+	{
+		if(data.message != null)
+		{
+			$scope.invEmail = "";
+			$scope.invEmail = data.message;
+		}
+	}
+}).error(function(error) {
+	console.log("error");
+});
+
+
+
+$scope.update_Profile = function()
+{
+	$scope.inv_phone = "";
+	$scope.inv_credit_card = "";
+
+
+
 		console.log("Inside update profile " + $scope.user_first_name);
 		console.log("bc profile pic");
-
-		$scope.profile_pic = $("#profile_pic").val();
-		console.log( $scope.profile_pic);
-		console.log("printing the card"+$scope.user_creditcard);
-		   
-	if (!($scope.user_creditcard.length == 16 || !isNaN($scope.user_creditcard)))  
-		{
-		 $scope.inv_credit_card = "invalid credit card number" ;
-		alert("invalid credit card");
-		$state.reload();
-
-		
-		}
-		
-	else 
-		
-		if (!($scope.phone.length == 10 && !isNaN($scope.phone)))
-		{
 			
+		
+		if ($("#profile_pic").val()){
+		$scope.profile_pic = $("#profile_pic").val();
+		}
+		
+		else
+			{
+			
+			console.log( $scope.profile_pic);
 
-			 $scope.inv_phone = "invalid phone number" ;
-			 alert("invalid phone number");
-				$state.reload();
+			}
+		
+		console.log("this is id"+$("#profile_pic").val());
+		console.log( $scope.profile_pic);
+		
+		if ($scope.user_creditcard.length == 16 && !isNaN($scope.user_creditcard))
+			{
+			console.log("correct credit card")
+			}
+		else
+			{
+			$scope.inv_credit_card = "Invalid credit card";
+			}
+		
+		if ($scope.phone.length == 10 && !isNaN($scope.phone))
+			{
+			console.log("corrct phone")
+			}
+		else
+			
+		{
+			$scope.inv_phone = "Invalid phone number";
 
 		}
 		
@@ -153,55 +97,59 @@ $scope.inv_credit_card = "";
 		
 		
 		
-		
-				  
-		else if($scope.user_creditcard.length == 16 && $scope.phone.length == 10 && !isNaN($scope.user_creditcard) && !isNaN($scope.phone) )
-		    {
-			  $scope.profile_pic = $("#profile_pic").val();
-		  
-		
-			$http({
-				method : "POST",
-				url : '/user/update_profile',
-				data : {
-					"user_first_name" : $scope.user_first_name,
-					"user_last_name" : $scope.user_last_name,
-					"user_sex" : $scope.sex,
-					"birthday" : $scope.birthday,
-					"user_email" : $scope.user_email,
-					"user_phone" : $scope.phone,
-					"user_preferred_locale" : $scope.user_preferred_locale,
-					"user_native_currency" :$scope.user_native_currency,
-					"user_city" : $scope.city,
-					"user_about":$scope.about,
-					"credit_card" : $scope.user_creditcard,
-					"profile_pic" :$scope.profile_pic
-				}
-			}).success(function(data){
-				if (data.statuscode == 0)
-				{
-					console.log("updated the profile successfully");
-				}
-				else
-				{
-					if(data.message != null)
-					{
-						$scope.invEmail = "";
-						$scope.invEmail = data.message;
-					}
-				}
-			}).error(function(error) {
-				console.log("error");
-			});
-		
-		    }
-		  else{
-		  alert("invalid card number or phone number");
-		  console.log("coming out from the update profile");
-		//  $scope.inv_credit_card = "invalid credit card number or mobile number. Kindly check";
+		if ( $scope.profile_pic != "" && $scope.user_creditcard.length == 16 && !isNaN($scope.user_creditcard) && $scope.phone.length == 10 && !isNaN($scope.phone))
 
-		  }
-	}
+		
+		{
+		$http({
+			method : "POST",
+			url : '/user/update_profile',
+			data : {
+				"user_first_name" : $scope.user_first_name,
+				"user_last_name" : $scope.user_last_name,
+				"user_sex" : $scope.sex,
+				"user_birthday" : $scope.user_birthday_month+ $scope.user_birthday_date+$scope.user_birthday_date,
+				"user_email" : $scope.user_email,
+				"user_phone" : $scope.phone,
+				"user_preferred_locale" : $scope.user_preferred_locale,
+				"user_native_currency" :$scope.user_native_currency,
+				"user_city" : $scope.city,
+				"user_about":$scope.about,
+				"credit_card" : $scope.user_creditcard,
+				"profile_pic" :$scope.profile_pic
+			}
+		}).success(function(data){
+			if (data.statuscode == 0)
+			{
+				console.log("updated the profile successfully");
+				alert("updated information successfully");
+			}
+			else
+			{
+				if(data.message != null)
+				{
+					$scope.invEmail = "";
+					$scope.invEmail = data.message;
+				}
+			}
+		}).error(function(error) {
+			console.log("error");
+		});
+	
+
+}
+		
+		else
+			{
+			alert("incorrect info");
+			
+			}
+
+
+
+
+}
+
 
 	$http({
 		method : "POST",
