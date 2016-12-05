5d52aedab328bc44c9454ed0ebe8d35a7024b686
Shrutiloya <shrutiloya1@gmail.com>
2016-12-04 21:16:59 -0800

    pin code validation

--- Airbnb-Client/public/scripts/controller.becomehost.location.js
+++ Airbnb-Client/public/scripts/controller.becomehost.location.js
@@ -7,7 +7,7 @@ airbnbApp.controller('controllerBecomeHostLocation',function($scope,$state,$log,
     $scope.city = "";
     $scope.state = "";
     $scope.zip = "";
-
+    $scope.invPin = true;
 
     $scope.invAddress = ""
     $scope.guessedAddMessage = "";
@@ -26,43 +26,42 @@ airbnbApp.controller('controllerBecomeHostLocation',function($scope,$state,$log,
         $scope.step2det.state = $scope.state;
         $scope.step2det.zip = $scope.zip;
         $scope.step2det.country = $scope.country;
-        
-         $http({
-                method : "POST",
-                url : '/host/validateaddress',
-                data :{
-                    address : address
+        var postalCodeRegex = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/;
+        var isValidPin = postalCodeRegex.test($scope.zip);
+        if(isValidPin) {
+            $http({
+                method: "POST",
+                url: '/host/validateaddress',
+                data: {
+                    address: address
                 }
-            }).success(function(data) {
-                if(data.statuscode == 0) 
-                {
-                    if(data.data.matchedAddress == "" && data.data.guessedAddress == "")
-                    {
+            }).success(function (data) {
+                if (data.statuscode == 0) {
+                    if (data.data.matchedAddress == "" && data.data.guessedAddress == "") {
                         $scope.invAddress = "Address you provided is Invalid";
                     }
-                    else if(data.data.matchedAddress != "" && data.data.guessedAddress == "")
-                    {
+                    else if (data.data.matchedAddress != "" && data.data.guessedAddress == "") {
                         var addrArray = data.data.matchedAddress.split(",");
                         $scope.step2det.street = addrArray[0].trim();
                         $scope.step2det.apt = $scope.apt;
                         $scope.step2det.city = addrArray[1].trim();
                         $scope.step2det.state = addrArray[2].trim();
                         $scope.step2det.zip = $scope.zip;
-                        $scope.step2det.country = addrArray[3].trim();   
-                        
+                        $scope.step2det.country = addrArray[3].trim();
+
 
                         //Auto Populate
-                        $scope.street =  $scope.step2det.street ;
-                        $scope.apt = $scope.step2det.apt ;
+                        $scope.street = $scope.step2det.street;
+                        $scope.apt = $scope.step2det.apt;
                         $scope.city = $scope.step2det.city;
                         $scope.state = $scope.step2det.state;
                         $scope.zip = $scope.step2det.zip;
                         $scope.country = $scope.step2det.country;
                         $scope.guessedAddMessage = "Did You Mean"
                         $scope.confirmedAddress = true;
+                        $scope.invPin = true;
                     }
-                    else if(data.data.matchedAddress == "" && data.data.guessedAddress != "")
-                    {
+                    else if (data.data.matchedAddress == "" && data.data.guessedAddress != "") {
                         var addrArray = data.data.guessedAddress.split(",");
                         $scope.step2det.street = addrArray[0].trim();
                         $scope.step2det.apt = $scope.apt;
@@ -73,28 +72,37 @@ airbnbApp.controller('controllerBecomeHostLocation',function($scope,$state,$log,
 
 
                         //Auto Populate
-                        $scope.street =  $scope.step2det.street ;
-                        $scope.apt = $scope.step2det.apt ;
+                        $scope.street = $scope.step2det.street;
+                        $scope.apt = $scope.step2det.apt;
                         $scope.city = $scope.step2det.city;
                         $scope.state = $scope.step2det.state;
                         $scope.zip = $scope.step2det.zip;
                         $scope.country = $scope.step2det.country;
                         $scope.guessedAddMessage = "Did You Mean";
                         $scope.confirmedAddress = true;
+                        $scope.invPin = true;
                     }
                 }
-                else 
-                {
+                else {
                     $scope.invMessage = data.message;
                 }
-            }).error(function(error) {
-                
+            }).error(function (error) {
+
                 $state.go('home.becomeHost')
-            });        
+            });
+            $scope.invPin = true;
+        }
+        else
+            {
+                $scope.invPin = false;
+
+        }
     };
 
 
-    $scope.becomeHost = function(){
+    $scope.becomeHost = function()
+    {
+
         if($scope.confirmedAddress)
         {
             var confAdd = $scope.step2det.street+", "+$scope.step2det.city+", "+$scope.step2det.state+", "+$scope.step2det.zip+", "+$scope.step2det.country;
